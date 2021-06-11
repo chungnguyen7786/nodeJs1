e366d047de1e6dcf1af38a6e3ed817b33a2b6f8c
chungnguyen7786 <themask7786@gmail.com>
2021-06-11 10:46:24 +0700

    use static files

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 4000
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.set('view engine', 'pug')
 app.set('views', './views')
